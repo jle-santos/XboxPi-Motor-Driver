83c94ea71633d852a4215991cc59e11aedbaed64
KarenMission <karen.mission@hotmail.com>
2020-07-27 13:43:22 -0700

    Integrated controls

--- motorpi.py
+++ motorpi.py
@@ -1,24 +1,47 @@
 # Controls Adafruit Motor Driver with Xbox Controls
 
+'''
+Motor Configuration
+{1} -FRONT- {2}
+    
+{3} - BACK- {4}
+
+'''
+
 from adafruit_motorkit import MotorKit
-import time
 import xbox
 
+# Create Motor Object
 kit = MotorKit()
 
-print("Hello world")
+# Create joystick object
+joy = xbox.Joystick()
 
-print("turning all motors")
+print("Starting Motor Control")
+print("Press BACK button to exit")
 
-kit.motor1.throttle = -1
-kit.motor2.throttle = -1
-kit.motor3.throttle = -1
-kit.motor4.throttle = -1
-
-time.sleep(2)
-
-kit.motor1.throttle = 0
-kit.motor2.throttle = 0
-kit.motor3.throttle = 0
-kit.motor4.throttle = 0
+while not joy.Back():
+    # Normalize joystick values
+    driveX = joy.leftX()
+    driveY = joy.leftY()
+    print("Left X/Y: ", '{0:.2}'.format(driveX), "/", '{0:.2}'.format(driveY), "DIR: ", end='')
+    # Check if the left stick is turning, else its forward/back
+    if driveX > 0.7:
+        print(" RIGHT"),
+        kit.motor1.throttle = driveX
+        kit.motor3.throttle = driveX
+        kit.motor2.throttle = -driveX # Spin opposite way
+        kit.motor4.throttle = -driveX # Spin opposite way
+    elif driveX < -0.3:
+        print(" LEFT"),
+        kit.motor1.throttle = -driveX
+        kit.motor3.throttle = -driveX
+        kit.motor2.throttle = driveX # Spin opposite way
+        kit.motor4.throttle = driveX # Spin opposite way
+    else:
+        print(" STRAIGHT"),
+        kit.motor1.throttle = driveY
+        kit.motor3.throttle = driveY
+        kit.motor2.throttle = driveY # Spin opposite way
+        kit.motor4.throttle = driveY # Spin opposite way
 
